a6687e84d4f39d4ce23ad29805f42553a3304054
seungpyo Hong <62321931+tv1039@users.noreply.github.com>
2024-06-11 21:43:03 +0900

    Add Unity files

---
+++ UnitiyiOSBuild/Build/Il2CppOutputProject/Source/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,53 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void ButtonClicker_OnButtonPressed_m25C4D61057BCC835AA0369F97B9DA96E7619CC99 (void);
+extern void ButtonClicker__ctor_m307CBF7349E89A76D4FFF319A551AA11797B56B2 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1 (void);
+static Il2CppMethodPointer s_methodPointers[4] = 
+{
+	ButtonClicker_OnButtonPressed_m25C4D61057BCC835AA0369F97B9DA96E7619CC99,
+	ButtonClicker__ctor_m307CBF7349E89A76D4FFF319A551AA11797B56B2,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1,
+};
+static const int32_t s_InvokerIndices[4] = 
+{
+	4190,
+	4190,
+	6084,
+	4190,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	4,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
